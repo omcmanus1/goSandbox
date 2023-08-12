5cae2135ba4cb23eeaaa1c6afca8cd524506ea99
Oliver McManus <omcmanus1@gmail.com>
2023-08-12 17:36:28 +0100

    random updates

--- internal/random/random.go
+++ internal/random/random.go
@@ -3,10 +3,12 @@ package random
 import (
 	"fmt"
 	"math/rand"
+	"github.com/fatih/color"
 )
 
 func Random() {
+	textColour := color.New(color.FgHiGreen)
 	output := fmt.Sprintf("Hello there for the %vth time", rand.Intn(20))
-	fmt.Println(output)
+	textColour.Println(output)
 	fmt.Printf("Hello there for the %vth time from random \n", rand.Intn(20))
 }
